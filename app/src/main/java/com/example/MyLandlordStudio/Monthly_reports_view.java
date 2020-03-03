698644c458c27326077194ae1417280b6206efcf
ELITEBOOK 840 <benkigera@gmail.com>
2020-03-03 15:26:48 +0300

    This is an update on the report fragments

---
+++ app/src/main/java/com/example/MyLandlordStudio/Monthly_reports_view.java
@@ -0,0 +1,14 @@
+package com.example.MyLandlordStudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Monthly_reports_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_reports_view);
+    }
+}
--- app/src/main/java/com/example/MyLandlordStudio/Reports_Fragment.java
+++ app/src/main/java/com/example/MyLandlordStudio/Reports_Fragment.java
@@ -1,5 +1,7 @@
 package com.example.MyLandlordStudio;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +9,43 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+
+
 public class Reports_Fragment extends Fragment {
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_reports,container,false);
+
+        //to use find View by id use container
+        View view=inflater.inflate(R.layout.fragment_reports,container,false);
+        CardView monthly_reports_cardView = view.findViewById(R.id.monthly_reports_cardview);
+
+        monthly_reports_cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              //intent from fragment to activity
+                Intent monthly_reports_intent=new Intent(getActivity(),Monthly_reports_view.class);
+                startActivity(monthly_reports_intent);
+
+            }
+        });
+        return view;
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
