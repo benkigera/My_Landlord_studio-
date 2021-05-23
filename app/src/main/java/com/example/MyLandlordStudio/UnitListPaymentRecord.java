a29dfa411ff5a993a4c23279a43fd0f950fc4d8e
ELITEBOOK 840 <benkigera@gmail.com>
2021-05-23 03:17:08 +0300

    progress

---
+++ app/src/main/java/com/example/MyLandlordStudio/UnitListPaymentRecord.java
@@ -0,0 +1,19 @@
+package com.example.MyLandlordStudio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class UnitListPaymentRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_list_payment_record);
+
+
+    }
+}
